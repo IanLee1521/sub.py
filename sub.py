ca50f2181b15478169ad8421aeda6676ad920d54
Ian Lee <lee1001@llnl.gov>
2017-09-03 18:09:45 -0700

    Removed whitespace before ":"

--- sub.py
+++ sub.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup as bs4
 # 46.166.129.226, 109.238.2.83, 176.193.213.61
 
 dmn = sys.argv[1]
-prx = urllib2.ProxyHandler({'http' : 'http://46.166.129.226:80'})
+prx = urllib2.ProxyHandler({'http': 'http://46.166.129.226:80'})
 opn = urllib2.build_opener(prx)
 urllib2.install_opener(opn)
 req = urllib2.urlopen('https://www.virustotal.com/en/domain/' + dmn + '/information')
@@ -17,7 +17,7 @@ res = req.read()
 sou = bs4(res, 'html.parser')
 fin = []
 
-for sdn in sou.findAll('a', {'target' : '_blank'}):
+for sdn in sou.findAll('a', {'target': '_blank'}):
     if dmn in sdn.text:
         fin.append(sdn.text.strip())
 
